59b83224a707dee21eae07ef671700720fbc1270
Rafael Espinoza <nerddiffer@gmail.com>
2017-01-25 23:38:41 -0800

    feat(clock): Add a basic clock module

---
+++ src/clock.js
@@ -0,0 +1,27 @@
+const { Countdown } = require('timepiece');
+const getDurationObj = require('./duration.js');
+
+// TODO: allow user to pick a time + date in the future
+const getDestinationTime = () => new Date(2017, 1, 1).valueOf(); // feb 1 2017
+const dest = getDestinationTime();
+
+const now = Date.now();
+const c = new Countdown(Math.floor((dest - now) / 1000));
+
+// DOM references
+const DAYS = document.getElementById('days');
+const HOURS = document.getElementById('hours');
+const MINUTES = document.getElementById('minutes');
+const SECONDS = document.getElementById('seconds');
+
+const updateClock = c => {
+  const { years, days, hours, minutes, seconds } = getDurationObj(c.remaining);
+
+  DAYS.innerHTML = days;
+  HOURS.innerHTML = hours;
+  MINUTES.innerHTML = minutes;
+  SECONDS.innerHTML = seconds;
+};
+
+window.updateClock = updateClock;
+window.c = c;
